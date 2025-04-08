b35ff15536bdc7d8aae527d8d3f483cd0f1b7f83
TranAQuyen <quyen2909a@gmail.com>
2025-04-08 17:24:00 +0700

    Post,get,put,delete Tour

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const User = require("../controllers/authController");
-const role = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post("/register",role.authenticateToken,role.authorizeRole(["admin","user"]), User.registerUser);
-router.post("/login", User.loginUser);
+
+router.post("/register", User.registerUser);
+router.post("/login",User.loginUser);
 
 module.exports = router;
