d808d148902272c585bdf5ef74e7580f86668988
TranAQuyen <quyen2909a@gmail.com>
2025-04-28 19:13:31 +0700

    them chuc nang chon tour theo dia diem

--- server/routes/tourRoutes.js
+++ server/routes/tourRoutes.js
@@ -6,6 +6,32 @@ const upload = require("../middlewares/upload");
 
 
 
-router.post("/", role.authenticateToken, role.authorizeRole(["provider","admin"]), upload.array("images"), Tour.createTour);
+router.post("/createTour", role.authenticateToken, role.authorizeRole(["provider","admin"]), upload.array("images"), Tour.createTour);
+router.get(
+  "/getTour",
+  role.authenticateToken,
+  role.authorizeRole(["provider", "admin"]),
+  Tour.getTour
+);
+
+router.put(
+  "/updateTour",
+  role.authenticateToken,
+  role.authorizeRole(["provider", "admin"]),
+  Tour.updateTour
+);
+
+router.delete(
+  "/deleteTour",
+  role.authenticateToken,
+  role.authorizeRole(["provider", "admin"]),
+  Tour.deleteTour
+);
+
+router.put(
+  "/findTour",
+  role.authenticateToken,
+  Tour.getToursByLocation
+);
 
 module.exports = router;
