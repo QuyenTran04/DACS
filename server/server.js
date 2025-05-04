497e696c8ae74de8e6e65598876e9a45197a4100
QuyenProVip <quyen2909a@gmail.com>
2025-05-05 00:57:16 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,12 +6,7 @@ const authRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const tourRoutes = require("./routes/tourRoutes");
-
 const locationRoutes = require("./routes/locationRoutes");
-
-const momoRoutes = require("./routes/momoRoutes");
-const bookingRoutes = require("./routes/bookingRoutes");
-
 const app = express();
 app.use(express.json());
 app.use(cors()); 
@@ -22,14 +17,8 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/profile", profileRoutes);
-
-app.use("/api/",tourRoutes);
-app.use("/api/locations", locationRoutes);
-
 app.use("/api/tour",tourRoutes);
-app.use("/api/momo", momoRoutes);
-app.use("/api/booking", bookingRoutes);
-
+app.use("/api/locations", locationRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server chạy trên cổng ${PORT}`));
