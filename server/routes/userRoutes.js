550069b6edfb89357083954add59d3895e30eacf
TranAQuyen <quyen2909a@gmail.com>
2025-05-04 22:09:05 +0700

    loi dieu huong

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const User = require("../controllers/authController");
-const role = require("../middlewares/authMiddleware");
-const router = express.Router();
-
-router.post("/register",role.authenticateToken,role.authorizeRole(["admin","user"]), User.registerUser);
-router.post("/login", User.loginUser);
-
-module.exports = router;
