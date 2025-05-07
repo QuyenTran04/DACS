273494c38641f7feb566769bf9932689811dfd27
TranAQuyen <quyen2909a@gmail.com>
2025-05-07 14:04:00 +0700

    tao dia chi

--- server/routes/locationRoutes.js
+++ server/routes/locationRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const locationController = require("../controllers/location");
 
-router.get("/", locationController.getAllLocations);
+router.post("/add", locationController.addLocations);
+router.get("/listLocation", locationController.getAllLocations);
 
 module.exports = router;
