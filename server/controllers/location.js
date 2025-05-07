4a6cbe11cc84e3627672c7fe781c4d80370adc8c
TranAQuyen <quyen2909a@gmail.com>
2025-05-07 14:04:00 +0700

    tao dia chi

--- server/controllers/location.js
+++ server/controllers/location.js
@@ -1,8 +1,35 @@
 const Location = require("../models/Location");
 
+exports.addLocations = async (req, res) => {
+  try {
+    const { name, country } = req.body; 
+    if (!name) {
+      return res
+        .status(400)
+        .json({ message: "Tên location không được để trống" });
+    }
+    const existingUser = await Location.findOne({ name });
+    if (existingUser) {
+      return res.status(400).json({ message: "Location đã tồn tại" });
+    }
+    const newLocation = new Location({
+      name,
+      country: country || "Việt Nam", 
+    });
+    await newLocation.save();
+    res
+      .status(201)
+      .json({ message: "Thêm location thành công", location: newLocation });
+  } catch (error) {
+    console.error("Lỗi tạo location:", error);
+    res.status(500).json({ message: "Lỗi server!" });
+  }
+};
+
+
 exports.getAllLocations = async (req, res) => {
   try {
-    const locations = await Location.find({}, "_id name"); 
+    const locations = await Location.find(); 
     res.status(200).json(locations);
   } catch (error) {
     console.error("Lỗi lấy danh sách location:", error);
