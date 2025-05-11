20d8c2b6b119018354aaf5dd2cdcd35aa068771e
TranAQuyen <quyen2909a@gmail.com>
2025-05-11 19:06:24 +0700

    sửa model tour

--- server/models/Location.js
+++ server/models/Location.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 
 const locationSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true }, 
-  country: { type: String, default: "Vietnam" },
-  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Location", locationSchema);
--- server/models/tour.js
+++ server/models/tour.js
@@ -1,18 +1,26 @@
 const mongoose = require("mongoose");
 
 const tourSchema = new mongoose.Schema({
-  providerId: { type: Number, ref: "Account", required: true },
   title: { type: String, required: true, trim: true },
   description: { type: String, required: true },
   address: { type: String, required: true },
-  location: {type: mongoose.Schema.Types.ObjectId, ref: "Location",required: true},
+  location: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Location",
+    required: true,
+  },
   price: { type: Number, required: true, min: 0 },
   duration: { type: Number, required: true },
-  images: [{ type: String }],
+  images: [{ data: Buffer, contentType: String }],
   availableDates: [{ type: Date }],
   maxGuests: { type: Number, required: true },
+  itinerary: [
+    {
+      time: { type: String, required: true },
+      activity: { type: String, required: true },
+    },
+  ],
   createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.models.Tour || mongoose.model("Tour", tourSchema);
-
